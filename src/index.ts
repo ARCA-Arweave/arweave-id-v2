bd3093b66462ebfe6f790d957bb4c6cc1890dc4a
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2020-06-04 18:36:15 -0400

    Export name properly

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { retrieveArweaveIdV1fromAddress } from './arweaveID';
 
-retrieveArweaveIdV1fromAddress("bLkyTRJCYg8WxBKrjBwAaRe1H7HYDfXzl7YKCENvw-Q").then(res => console.log("Say Cheese " + res));
+export = retrieveArweaveIdV1fromAddress;
